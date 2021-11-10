6c2402ece5b1509ee5db8daa20e6285f840c63fe
NoP77svk <nop77svk@gmail.com>
2021-11-10 13:26:56 +0100

    ➕ query benchmark (ADO.NET) utility

---
+++ tdv_query_benchmark/CommandLineOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace NoP77svk.TibcoDV.QueryBenchmark
+{
+    using System.Collections.Generic;
+    using CommandLine;
+    using NoP77svk.TibcoDV.Commons;
+
+    internal class CommandLineOptions
+        : BaseCLI
+    {
+        [Option('f', "script", Required = true, Separator = ',', HelpText = "\n"
+            + "A comma-delimited list of \"SQL\" scripts")]
+        public IEnumerable<string?>? PrivilegeDefinitionFiles { get; set; }
+    }
+}
---
+++ tdv_query_benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace NoP77svk.TibcoDV.QueryBenchmark
+{
+    using System;
+    using System.Data.CompositeClient;
+    using NoP77svk.TibcoDV.Commons;
+
+    class Program : BaseCLI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
