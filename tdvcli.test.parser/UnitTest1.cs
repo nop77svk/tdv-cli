322a78e4538dc4a968fbb9493fc7520cd444898a
pierre@audrey <nop77svk@gmail.com>
2021-11-11 20:41:19 +0100

    ➕ test suite (stub) for TDV CLI PEG

---
+++ tdvcli.test.parser/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace tdvcli.test.parser
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
