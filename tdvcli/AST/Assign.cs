568e7cface4b974fa8aa23916211e96086b20cb2
NoP77svk <nop77svk@gmail.com>
2021-11-16 13:51:41 +0100

    🛠 AST.Assign switched to concise record syntax

--- tdvcli/AST/Assign.cs
+++ tdvcli/AST/Assign.cs
@@ -1,10 +1,7 @@
-﻿namespace NoP77svk.TibcoDV.CLI.AST
+﻿#pragma warning disable SA1313
+namespace NoP77svk.TibcoDV.CLI.AST
 {
     using WSDL = NoP77svk.TibcoDV.API.WSDL.Admin;
 
-    internal record Assign
-    {
-        public WSDL.rbsAssignmentOperationType Action { get; init; }
-        public AssignWhat? What { get; init; }
-    }
+    internal record Assign(WSDL.rbsAssignmentOperationType Action, AssignWhat What);
 }
