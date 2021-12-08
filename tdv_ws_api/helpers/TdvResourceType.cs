fdd5284f90ab735b1b1ca0763c736702ffdbb7f3
NoP77svk <nop77svk@gmail.com>
2021-12-08 14:50:33 +0100

    * TDV resource type remapper revamped for better usage patterns

--- tdv_ws_api/helpers/TdvResourceType.cs
+++ tdv_ws_api/helpers/TdvResourceType.cs
@@ -3,9 +3,29 @@ namespace NoP77svk.TibcoDV.API
 {
     using System;
 
-    public record TdvResourceType(string? WsType, string? WsSubType, string? WsTargetType = null)
+    public record TdvResourceType
     {
-        public TdvResourceTypeEnumAgr Type { get => CalcResourceType(WsType, WsSubType, WsTargetType); }
+        public string WsType { get; }
+        public string? WsSubType { get; }
+        public string? WsTargetType { get; }
+
+        public TdvResourceTypeEnumAgr Type { get; }
+
+        public TdvResourceType(string wsType, string? wsSubType, string? wsTargetType = null)
+        {
+            WsType = wsType;
+            WsSubType = wsSubType;
+            WsTargetType = wsTargetType;
+
+            Type = CalcResourceType(WsType, WsSubType, WsTargetType);
+        }
+
+        public TdvResourceType(TdvResourceTypeEnumAgr type)
+        {
+            Type = type;
+
+            (WsType, WsSubType, WsTargetType) = CalcWsResourceTypes(Type);
+        }
 
         /// <see cref="https://docs.tibco.com/pub/tdv/8.4.0/doc/html/StudioHelp/index.html#page/StudioHelp/Ch_3_OperationsList.TDV%2520Resource%2520Types%2520and%2520Subtypes.html"/>
         public static TdvResourceTypeEnumAgr CalcResourceType(string? wsType, string? wsSubType, string? wsTargetType)
@@ -34,24 +54,24 @@ namespace NoP77svk.TibcoDV.API
             };
         }
 
-        public static ValueTuple<string, string> CalcWsResourceTypes(TdvResourceTypeEnumAgr type)
+        public static ValueTuple<string, string?, string?> CalcWsResourceTypes(TdvResourceTypeEnumAgr type)
         {
             return type switch
             {
-                TdvResourceTypeEnumAgr.Folder => new (TdvResourceTypeConst.Container, TdvResourceSubtypeConst.FolderContainer),
-                TdvResourceTypeEnumAgr.PublishedCatalog => new (TdvResourceTypeConst.Container, TdvResourceSubtypeConst.CatalogContainer),
-                TdvResourceTypeEnumAgr.PublishedSchema => new (TdvResourceTypeConst.Container, TdvResourceSubtypeConst.SchemaContainer),
-                TdvResourceTypeEnumAgr.DataSourceRelational => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.RelationalDataSource),
-                TdvResourceTypeEnumAgr.DataSourceCompositeWebService => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.CompositeWebService),
-                TdvResourceTypeEnumAgr.DataSourceFile => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.FileDataSource),
-                TdvResourceTypeEnumAgr.DataSourceExcel => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.PoiExcelDataSource),
-                TdvResourceTypeEnumAgr.DataSourceXmlFile => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.XmlFileDataSource),
-                TdvResourceTypeEnumAgr.DataSourceWsWsdl => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.WsdlDataSource),
-                TdvResourceTypeEnumAgr.Table => new (TdvResourceTypeConst.Table, TdvResourceSubtypeConst.DatabaseTable),
-                TdvResourceTypeEnumAgr.View => new (TdvResourceTypeConst.Table, TdvResourceSubtypeConst.SqlTable),
-                TdvResourceTypeEnumAgr.StoredProcedureSQL => new (TdvResourceTypeConst.Procedure, TdvResourceSubtypeConst.SqlScriptProcedure),
-                TdvResourceTypeEnumAgr.PublishedTableOrView => new (TdvResourceTypeConst.Link, TdvResourceSubtypeConst.None),
-                TdvResourceTypeEnumAgr.Model => new (TdvResourceTypeConst.Model, TdvResourceSubtypeConst.None),
+                TdvResourceTypeEnumAgr.Folder => new (TdvResourceTypeConst.Container, TdvResourceSubtypeConst.FolderContainer, null),
+                TdvResourceTypeEnumAgr.PublishedCatalog => new (TdvResourceTypeConst.Container, TdvResourceSubtypeConst.CatalogContainer, null),
+                TdvResourceTypeEnumAgr.PublishedSchema => new (TdvResourceTypeConst.Container, TdvResourceSubtypeConst.SchemaContainer, null),
+                TdvResourceTypeEnumAgr.DataSourceRelational => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.RelationalDataSource, null),
+                TdvResourceTypeEnumAgr.DataSourceCompositeWebService => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.CompositeWebService, null),
+                TdvResourceTypeEnumAgr.DataSourceFile => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.FileDataSource, null),
+                TdvResourceTypeEnumAgr.DataSourceExcel => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.PoiExcelDataSource, null),
+                TdvResourceTypeEnumAgr.DataSourceXmlFile => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.XmlFileDataSource, null),
+                TdvResourceTypeEnumAgr.DataSourceWsWsdl => new (TdvResourceTypeConst.DataSource, TdvResourceSubtypeConst.WsdlDataSource, null),
+                TdvResourceTypeEnumAgr.Table => new (TdvResourceTypeConst.Table, TdvResourceSubtypeConst.DatabaseTable, null),
+                TdvResourceTypeEnumAgr.View => new (TdvResourceTypeConst.Table, TdvResourceSubtypeConst.SqlTable, null),
+                TdvResourceTypeEnumAgr.StoredProcedureSQL => new (TdvResourceTypeConst.Procedure, TdvResourceSubtypeConst.SqlScriptProcedure, null),
+                TdvResourceTypeEnumAgr.PublishedTableOrView => new (TdvResourceTypeConst.Link, TdvResourceSubtypeConst.None, null),
+                TdvResourceTypeEnumAgr.Model => new (TdvResourceTypeConst.Model, TdvResourceSubtypeConst.None, null),
                 _ => throw new ArgumentOutOfRangeException(nameof(type), $"Unrecognized resource type \"{type}\"")
             };
         }
