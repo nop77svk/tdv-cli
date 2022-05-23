8ccc046960ae1528cacc6f974de9ecb2c3a2dc34
NoP77svk <nop77svk@gmail.com>
2022-05-23 09:00:34 +0200

    ➕ "introspection" API group stub

---
+++ tdv_ws_api/WS API groups/Introspect.cs
@@ -0,0 +1,12 @@
+﻿namespace NoP77svk.TibcoDV.API
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public partial class TdvWebServiceClient
+    {
+    }
+}
