c48bc51c60b5b636bb6dd037dceaee102d053c6c
NoP77svk <nop77svk@gmail.com>
2022-05-23 11:24:30 +0200

    🧹 code style fixed

--- tdv_ws_api/WS API groups/Introspect.cs
+++ tdv_ws_api/WS API groups/Introspect.cs
@@ -8,31 +8,6 @@
 
     public partial class TdvWebServiceClient
     {
-        public async Task<int> GetIntrospectedResourceIdsTask(string dataSourcePath)
-        {
-            IAsyncEnumerable<WSDL.Admin.getIntrospectedResourceIdsTaskResponse> response = _wsClient.EndpointGetObject<WSDL.Admin.getIntrospectedResourceIdsTaskResponse>(
-                new TdvSoapWsEndpoint<WSDL.Admin.getIntrospectedResourceIdsTaskRequest>("getIntrospectedResourceIdsTask", new WSDL.Admin.getIntrospectedResourceIdsTaskRequest()
-                {
-                    path = dataSourcePath
-                }
-            ));
-
-            WSDL.Admin.getIntrospectedResourceIdsTaskResponse result = await response.FirstAsync();
-            return int.Parse(result.taskId);
-        }
-
-        public async Task<WSDL.Admin.getIntrospectedResourceIdsResultResponse> GetIntrospectedResourceIdsResult(int taskId)
-        {
-            IAsyncEnumerable<WSDL.Admin.getIntrospectedResourceIdsResultResponse> response = _wsClient.EndpointGetObject<WSDL.Admin.getIntrospectedResourceIdsResultResponse>(
-                new TdvSoapWsEndpoint<WSDL.Admin.getIntrospectedResourceIdsResultRequest>("getIntrospectedResourceIdsResult", new WSDL.Admin.getIntrospectedResourceIdsResultRequest()
-                {
-                    taskId = taskId.ToString()
-                }
-            ));
-
-            return await response.FirstAsync();
-        }
-
         public async Task<WSDL.Admin.getIntrospectedResourceIdsResultResponse> GetIntrospectedResourceIds(string dataSourcePath, int pollingIntervalMS = 500, CancellationToken? cancellationToken = null)
         {
             if (pollingIntervalMS < 0)
@@ -54,6 +29,31 @@
             }
 
             return result;
+        }
+
+        private async Task<int> GetIntrospectedResourceIdsTask(string dataSourcePath)
+        {
+            IAsyncEnumerable<WSDL.Admin.getIntrospectedResourceIdsTaskResponse> response = _wsClient.EndpointGetObject<WSDL.Admin.getIntrospectedResourceIdsTaskResponse>(
+                new TdvSoapWsEndpoint<WSDL.Admin.getIntrospectedResourceIdsTaskRequest>("getIntrospectedResourceIdsTask", new WSDL.Admin.getIntrospectedResourceIdsTaskRequest()
+                {
+                    path = dataSourcePath
+                }
+            ));
+
+            WSDL.Admin.getIntrospectedResourceIdsTaskResponse result = await response.FirstAsync();
+            return int.Parse(result.taskId);
+        }
+
+        private async Task<WSDL.Admin.getIntrospectedResourceIdsResultResponse> GetIntrospectedResourceIdsResult(int taskId)
+        {
+            IAsyncEnumerable<WSDL.Admin.getIntrospectedResourceIdsResultResponse> response = _wsClient.EndpointGetObject<WSDL.Admin.getIntrospectedResourceIdsResultResponse>(
+                new TdvSoapWsEndpoint<WSDL.Admin.getIntrospectedResourceIdsResultRequest>("getIntrospectedResourceIdsResult", new WSDL.Admin.getIntrospectedResourceIdsResultRequest()
+                {
+                    taskId = taskId.ToString()
+                }
+            ));
+
+            return await response.FirstAsync();
         }
     }
 }
