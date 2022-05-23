e3177ee46603cc8cab69bc422a89024a12ac45c8
NoP77svk <nop77svk@gmail.com>
2022-05-23 11:10:32 +0200

    + "get introspected resource ids" async API

--- tdv_ws_api/WS API groups/Introspect.cs
+++ tdv_ws_api/WS API groups/Introspect.cs
@@ -3,10 +3,57 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
+    using System.Threading;
     using System.Threading.Tasks;
 
     public partial class TdvWebServiceClient
     {
+        public async Task<int> GetIntrospectedResourceIdsTask(string dataSourcePath)
+        {
+            IAsyncEnumerable<WSDL.Admin.getIntrospectedResourceIdsTaskResponse> response = _wsClient.EndpointGetObject<WSDL.Admin.getIntrospectedResourceIdsTaskResponse>(
+                new TdvSoapWsEndpoint<WSDL.Admin.getIntrospectedResourceIdsTaskRequest>("getIntrospectedResourceIdsTask", new WSDL.Admin.getIntrospectedResourceIdsTaskRequest()
+                {
+                    path = dataSourcePath
+                }
+            ));
+
+            WSDL.Admin.getIntrospectedResourceIdsTaskResponse result = await response.FirstAsync();
+            return int.Parse(result.taskId);
+        }
+
+        public async Task<WSDL.Admin.getIntrospectedResourceIdsResultResponse> GetIntrospectedResourceIdsResult(int taskId)
+        {
+            IAsyncEnumerable<WSDL.Admin.getIntrospectedResourceIdsResultResponse> response = _wsClient.EndpointGetObject<WSDL.Admin.getIntrospectedResourceIdsResultResponse>(
+                new TdvSoapWsEndpoint<WSDL.Admin.getIntrospectedResourceIdsResultRequest>("getIntrospectedResourceIdsResult", new WSDL.Admin.getIntrospectedResourceIdsResultRequest()
+                {
+                    taskId = taskId.ToString()
+                }
+            ));
+
+            return await response.FirstAsync();
+        }
+
+        public async Task<WSDL.Admin.getIntrospectedResourceIdsResultResponse> GetIntrospectedResourceIds(string dataSourcePath, int pollingIntervalMS = 500, CancellationToken? cancellationToken = null)
+        {
+            if (pollingIntervalMS < 0)
+                throw new ArgumentOutOfRangeException(nameof(pollingIntervalMS), pollingIntervalMS, "Invalid polling interval");
+
+            int taskId = await GetIntrospectedResourceIdsTask(dataSourcePath);
+
+            WSDL.Admin.getIntrospectedResourceIdsResultResponse result;
+            while (true)
+            {
+                result = await GetIntrospectedResourceIdsResult(taskId);
+                if (result.completed)
+                    break;
+
+                cancellationToken?.ThrowIfCancellationRequested();
+
+                if (pollingIntervalMS > 0)
+                    await Task.Delay(pollingIntervalMS);
+            }
+
+            return result;
+        }
     }
 }
