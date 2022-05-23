16556bcfb9c0808a1588f52d54fb9ee95a53d6ff
NoP77svk <nop77svk@gmail.com>
2022-05-23 09:00:20 +0200

    ➕ "transactions" API group stub

---
+++ tdv_ws_api/WS API groups/Transaction.cs
@@ -0,0 +1,21 @@
+﻿namespace NoP77svk.TibcoDV.API
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public partial class TdvWebServiceClient
+    {
+        public async Task<int> BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CloseTransaction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
