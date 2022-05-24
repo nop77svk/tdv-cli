4025deccf1d016dbb3a77314a7e1c4e341b6c1d7
NoP77svk <nop77svk@gmail.com>
2022-05-24 13:19:11 +0200

    ➕ "session" APIs

---
+++ tdv_ws_api/WS API groups/Session.cs
@@ -0,0 +1,28 @@
+﻿namespace NoP77svk.TibcoDV.API
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public partial class TdvWebServiceClient
+    {
+        public async Task<string> BeginSession()
+        {
+            IAsyncEnumerable<WSDL.Util.beginSessionResponse> response = _wsClient.EndpointGetObject<WSDL.Util.beginSessionResponse>(
+                new TdvSoapWsEndpoint<WSDL.Util.beginSessionRequest>("beginSession", new WSDL.Util.beginSessionRequest())
+            );
+
+            WSDL.Util.beginSessionResponse result = await response.FirstAsync();
+            return result.sessionToken;
+        }
+
+        public async Task CloseSession()
+        {
+            IAsyncEnumerable<WSDL.Util.closeSessionResponse> response = _wsClient.EndpointGetObject<WSDL.Util.closeSessionResponse>(
+                new TdvSoapWsEndpoint<WSDL.Util.closeSessionRequest>("closeSession", new WSDL.Util.closeSessionRequest())
+            );
+
+            await response.LastAsync();
+        }
+    }
+}
