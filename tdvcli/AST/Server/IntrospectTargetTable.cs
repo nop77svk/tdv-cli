5f468ad95dba28bf220f71019c81cf1c714a3f22
NoP77svk <nop77svk@gmail.com>
2022-05-27 12:17:17 +0200

    🛠 introspection subcategories now allow for regexp-matched identifiers

--- tdvcli/AST/Server/IntrospectTargetTable.cs
+++ tdvcli/AST/Server/IntrospectTargetTable.cs
@@ -3,7 +3,7 @@ namespace NoP77svk.TibcoDV.CLI.AST.Server
 {
     using NoP77svk.TibcoDV.CLI.AST.Infra;
 
-    internal record IntrospectTargetTable(SetElementOperation ElementOperation, MatchBy Match)
+    internal record IntrospectTargetTable(SetElementOperation ElementOperation, MatchBy TableName)
     {
     }
 }
