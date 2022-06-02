a5022e029763a714d8b44c15bca8a75eecc953ad
NoP77svk <nop77svk@gmail.com>
2022-06-02 15:01:08 +0200

    ➕ "introspect" command now retrieves list of all introspectable resources

--- tdvcli/AST/Server/CommandIntrospect.cs
+++ tdvcli/AST/Server/CommandIntrospect.cs
@@ -7,6 +7,7 @@
     using System.Threading.Tasks;
     using NoP77svk.TibcoDV.API;
     using NoP77svk.TibcoDV.CLI.Commons;
+    using WSDL = NoP77svk.TibcoDV.API.WSDL;
 
     internal class CommandIntrospect : IAsyncExecutable
     {
@@ -19,6 +20,27 @@
 
         public async Task Execute(TdvWebServiceClient tdvClient, IInfoOutput output, ParserState parserState)
         {
+            output.Info($"Introspecting {DataSources.Count} data sources...");
+
+            string[] uniqueDataSourcePaths = DataSources
+                .Select(x => x.DataSourcePath)
+                .Distinct()
+                .ToArray();
+
+            Task[] multiClearIntrospectableResourceIdCache = uniqueDataSourcePaths
+                .Select(dataSourcePath => tdvClient.ClearIntrospectableResourceIdCache(dataSourcePath))
+                .ToArray();
+            await Task.WhenAll();
+            output.Info("... introspectable resource cache cleared");
+
+            Task<WSDL.Admin.linkableResourceId[]>[] multiGetIntrospectableResources = uniqueDataSourcePaths
+                .Select(dataSourcePath => new API.PolledServerTasks.GetIntrospectableResourceIdsPolledServerTaskHandler(tdvClient, dataSourcePath, false))
+                .Select(taskHandler => tdvClient.PolledServerTaskEnumerable(taskHandler))
+                .Select(task => task.ToArrayAsync().AsTask())
+                .ToArray();
+            await Task.WhenAll(multiGetIntrospectableResources);
+            output.Info("... introspectable resource list retrieved");
+
             throw new NotImplementedException();
         }
 
