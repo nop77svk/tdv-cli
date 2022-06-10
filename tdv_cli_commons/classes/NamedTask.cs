c2ab05e62a695730786b2c81479c5fb60dab4a07
NoP77svk <nop77svk@gmail.com>
2022-06-10 08:50:00 +0200

    🛠 named task helper made IDisposable

--- tdv_cli_commons/classes/NamedTask.cs
+++ tdv_cli_commons/classes/NamedTask.cs
@@ -1,9 +1,14 @@
 ﻿#pragma warning disable SA1313
 namespace NoP77svk.TibcoDV.CLI.Commons
 {
+    using System;
     using System.Threading.Tasks;
 
-    public record NamedTask<TTaskResult>(string Name, Task<TTaskResult> Task)
+    public record NamedTask<TTaskResult>(string Name, Task<TTaskResult> Task) : IDisposable
     {
+        public void Dispose()
+        {
+            Task.Dispose();
+        }
     }
 }
