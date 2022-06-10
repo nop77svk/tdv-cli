068ab1353d1824ccc4ff2f992951b4823bfce57c
NoP77svk <nop77svk@gmail.com>
2022-06-10 08:49:40 +0200

    🛠 introspection progress helper made comparable

--- tdvcli/AST/Internal/IntrospectionProgress.cs
+++ tdvcli/AST/Internal/IntrospectionProgress.cs
@@ -1,11 +1,13 @@
 ﻿#pragma warning disable SA1401
 namespace NoP77svk.TibcoDV.CLI.AST.Internal
 {
-    internal class IntrospectionProgress
+    using System;
+
+    internal class IntrospectionProgress : IEquatable<IntrospectionProgress>
     {
-        internal int JobsRunning = 0;
+        internal int JobsSpawned = 0;
         internal int JobsDone = 0;
-        internal int JobsTotal = 0;
+        internal int JobsTotalToBeSpawned = 0;
         internal int Added = 0;
         internal int ToBeAdded = 0;
         internal int Updated = 0;
@@ -17,7 +19,24 @@ namespace NoP77svk.TibcoDV.CLI.AST.Internal
         internal int Errors = 0;
 
         internal int ObjectsProcessed { get => Added + Updated + Removed + Skipped + Warnings + Errors + JobsDone; }
-        internal int ObjectsTotal { get => ToBeAdded + ToBeUpdated + ToBeRemoved + JobsTotal; }
-        internal float ProgressPct { get => ObjectsTotal > 0 && JobsRunning == JobsTotal ? (float)ObjectsProcessed / ObjectsTotal : 0.0f; }
+        internal int ObjectsTotal { get => ToBeAdded + ToBeUpdated + ToBeRemoved + JobsTotalToBeSpawned; }
+        internal float ProgressPct { get => ObjectsTotal > 0 && JobsSpawned == JobsTotalToBeSpawned ? (float)ObjectsProcessed / ObjectsTotal : 0.0f; }
+
+        public bool Equals(IntrospectionProgress? other)
+        {
+            return JobsSpawned == other?.JobsSpawned
+                && JobsDone == other?.JobsDone
+                && JobsTotalToBeSpawned == other?.JobsTotalToBeSpawned
+                && Added == other?.Added
+                && ToBeAdded == other?.ToBeAdded
+                && Updated == other?.Updated
+                && ToBeUpdated == other?.ToBeUpdated
+                && Removed == other?.Removed
+                && ToBeRemoved == other?.ToBeRemoved
+                && Skipped == other?.Skipped
+                && Warnings == other?.Warnings
+                && Errors == other?.Errors
+            ;
+        }
     }
 }
