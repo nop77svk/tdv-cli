69fd924b33bf3a4e84cb44539e489993da83ba13
NoP77svk <nop77svk@gmail.com>
2022-06-15 22:22:22 +0200

    🔧 introspection progress' jobs done revamped a bit for better clarity

--- tdvcli/AST/Server/CommandIntrospect.cs
+++ tdvcli/AST/Server/CommandIntrospect.cs
@@ -385,7 +385,7 @@
                         else
                         {
                             output.InfoCR($"{overallProgress.ProgressPct:#####0%} done ("
-                                + $"{overallProgress.JobsRunning}+{overallProgress.JobsDone}/{overallProgress.JobsTotalToBeSpawned} jobs"
+                                + $"{overallProgress.JobsRunning}/{overallProgress.JobsTotalToBeSpawned}(-{overallProgress.JobsDone}) jobs"
                                 + (overallProgress.ToBeAdded > 0 ? $", add:{overallProgress.Added}/{overallProgress.ToBeAdded}" : string.Empty)
                                 + (overallProgress.ToBeUpdated > 0 ? $", upd:{overallProgress.Updated}(+{overallProgress.Skipped})/{overallProgress.ToBeUpdated}" : string.Empty)
                                 + (overallProgress.ToBeRemoved > 0 ? $", del:{overallProgress.Removed}/{overallProgress.ToBeRemoved}" : string.Empty)
